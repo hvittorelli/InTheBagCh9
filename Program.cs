ad9251070b84cf5ba0c93a4e626884b6c8209cda
hvittorelli <misshopev@gmail.com>
2023-10-31 19:39:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+namespace InTheBag
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSession();
+            builder.Services.AddSession(options => 
+                {
+                // Change idle timeout to 5 mins instead of the default (20 mins)
+                options.IdleTimeout = TimeSpan.FromSeconds(60 * 5);
+                // Turn on option for cookies
+                options.Cookie.IsEssential = true;
+                });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
